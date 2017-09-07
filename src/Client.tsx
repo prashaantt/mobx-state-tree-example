efb0b707eb55f32329f3d7284a36c26b194e2245
Prashant Tiwari <prashaantt@gmail.com>
2017-09-07 20:02:35 +0530

    Add devtools

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import DevTools from 'mobx-react-devtools';
 
 import App from "./components/App";
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <div>
+            <App />
+            <DevTools />
+        </div>
     </BrowserRouter>,
     document.getElementById("app")
 )
